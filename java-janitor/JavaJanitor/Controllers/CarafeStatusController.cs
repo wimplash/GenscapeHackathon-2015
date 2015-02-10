e3fbc48fdf8f73ac94aad3b359ff4a6e62c8da4e
Ryan Ransford <rdransfo@gmail.com>
2015-02-10 17:11:12 -0500

    adding CarafeStatus and CarafeStatusController

--- java-janitor/JavaJanitor/Controllers/CarafeStatusController.cs
+++ java-janitor/JavaJanitor/Controllers/CarafeStatusController.cs
@@ -4,36 +4,76 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using JavaJanitor.Models;
 
 namespace JavaJanitor.Controllers
 {
     public class CarafeStatusController : ApiController
     {
+        private List<CarafeStatus> Stats = new List<CarafeStatus>();
+
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IEnumerable<int> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Stats.Select(e => e.Id);
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public CarafeStatus Get(int id)
         {
-            return "value";
+            IEnumerable<CarafeStatus> matches = Stats.Where(e => e.Id == id);
+            if (matches.Count() == 0)
+            {
+                // return 404
+                return null;
+            }
+            else
+            {
+                return matches.First();
+            }
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public void Post([FromBody] CarafeStatus newStat)
         {
+            IEnumerable<CarafeStatus> matches = Stats.Where(e => e.Id == newStat.Id);
+            if (matches.Count() == 0)
+            {
+                Stats.Add(newStat);
+            }
+            else
+            {
+                // return appropriate status code
+            }
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] CarafeStatus newStat)
         {
+            IEnumerable<CarafeStatus> matches = Stats.Where(e => e.Id == id);
+            if (matches.Count() == 0)
+            {
+                // return 404
+            }
+            else
+            {
+                Stats.Remove(matches.First());
+                Stats.Add(newStat);
+            }
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+            IEnumerable<CarafeStatus> matches = Stats.Where(e => e.Id == id);
+            if (matches.Count() == 0)
+            {
+                // return 404
+            }
+            else
+            {
+                Stats.Remove(matches.First());
+            }
         }
     }
 }
---
+++ java-janitor/JavaJanitor/Models/CarafeStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace JavaJanitor.Models
+{
+    public enum Status { Empty, Full };
+
+    [JsonObject("carafe-status")]
+    public class CarafeStatus
+    {
+        public int Id { get; set; }
+        public Status Status { get; set; }
+    }
+}
