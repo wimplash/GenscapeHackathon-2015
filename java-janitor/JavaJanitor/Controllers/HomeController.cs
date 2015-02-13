00edeecf13aad6ab7e7bb82c4c1dcce8700e3cd8
Ryan Ransford <rdransfo@gmail.com>
2015-02-13 01:53:57 -0500

    Adding initial offender/offence values from controller

--- java-janitor/JavaJanitor/Controllers/HomeController.cs
+++ java-janitor/JavaJanitor/Controllers/HomeController.cs
@@ -13,6 +13,9 @@ namespace JavaJanitor.Controllers
             ViewBag.Title = "Genscape's Virtual Terry Tate";
             ViewBag.CarafeState = CarafeController.Carafe.Status;
             ViewBag.CarafeLastUpdated = CarafeController.Carafe.LastUpdated;
+            ViewBag.OffenderImage = CarafeController.Carafe.Image.Filename;
+            ViewBag.OffenderName = CarafeController.Carafe.Name;
+            ViewBag.OffenderLastUpdated = CarafeController.Carafe.Image.Timestamp;
             return View();
         }
     }
