6b4d700c5c7af4e04dbd61a6a0b54bb3238fe7c5
Ryan Ransford <rdransfo@gmail.com>
2015-02-13 02:03:17 -0500

    fixing more minor issues with HomeController

--- java-janitor/JavaJanitor/Controllers/HomeController.cs
+++ java-janitor/JavaJanitor/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JavaJanitor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,11 +12,16 @@ namespace JavaJanitor.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Genscape's Virtual Terry Tate";
-            ViewBag.CarafeState = CarafeController.Carafe.Status;
-            ViewBag.CarafeLastUpdated = CarafeController.Carafe.LastUpdated;
-            ViewBag.OffenderImage = CarafeController.Carafe.Image.Filename;
-            ViewBag.OffenderName = CarafeController.Carafe.Name;
-            ViewBag.OffenderLastUpdated = CarafeController.Carafe.Image.Timestamp;
+            Carafe carafe = CarafeController.Carafe;
+            ViewBag.CarafeState = carafe.Status;
+            ViewBag.CarafeLastUpdated = carafe.LastUpdated;
+            ViewBag.OffenderName = carafe.Name;
+            Image image = carafe.Image;
+            if (image != null)
+            {
+                ViewBag.OffenderImage = image.Filename;
+                ViewBag.OffenderLastUpdated = image.Timestamp;
+            }
             return View();
         }
     }
